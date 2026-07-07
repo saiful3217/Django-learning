c17df640bc9026842134bf706cea98c0548b2895
Saiful Islam Piash <saiful3217islam@gmail.com>
2026-07-07 23:58:50 +0200

    change in the views of app

--- project_3/app_3/views.py
+++ project_3/app_3/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     d={'author': 'Rahim', 'age':8  ,'l':[1,2,3,34,4,] }
-    return render(request,'app_3/home.html',d)
+    e={'author': 'aziz', 'age':70  ,'l':[91,29,38,34,44,] }
+    return render(request,'app_3/home.html',{'d': d,'e':e})
