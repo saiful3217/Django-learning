42f5eb5114b33007a75952a67d3151be139bf2eb
Saiful Islam Piash <saiful3217islam@gmail.com>
2026-07-21 03:00:30 +0200

    I am learning hiw to take input from user using html and Django api form

---
+++ learnForms/form_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from . forms import contactForm
+# Create your views here.
+def home(request):
+    return render(request,'form_app/home.html')
+def about(request):
+    if request.method == "POST":
+        name= request.POST.get('userName')
+        email=request.POST.get('email')
+        ratings=request.POST.get('ratings')
+        return render(request,'form_app/about.html',{'name':name,'email':email, 'ratings':ratings })
+    else:
+        return render(request,'form_app/about.html')
+
+def form(request):
+    return render(request,'form_app/form.html')
+
+def djangoForm(request):
+    form=contactForm()
+    return render(request,'form_app/django_form.html',{'form': form})
+    
