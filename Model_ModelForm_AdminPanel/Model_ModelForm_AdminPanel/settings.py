8ffd42245ada518c3fb7357ce7b5a33ffe9c62bc
Saiful Islam Piash <saiful3217islam@gmail.com>
2026-07-24 03:06:20 +0200

    covered Data showing in front end and Deletation from database

--- Model_ModelForm_AdminPanel/Model_ModelForm_AdminPanel/settings.py
+++ Model_ModelForm_AdminPanel/Model_ModelForm_AdminPanel/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'model_app'
+    'model_app',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -116,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- Model_ModelForm_AdminPanel/model_app/views.py
+++ Model_ModelForm_AdminPanel/model_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . import models
 # Create your views here.
+def home(request):
+    data= models.Student.objects.all()
+    print(data)
+    return render(request,'home.html',{'datas': data})
+
+
+def delete_student(request,roll):
+    std=models.Student.objects.get(pk=roll)
+    std.delete()
+    return redirect('homepage')
+    
